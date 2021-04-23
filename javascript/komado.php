a85fb07021719f34262998052782d041481fa45e
takahumip7 <72800775+takahumip7@users.noreply.github.com>
2021-04-23 23:22:15 +0900

    Add files via upload

---
+++ javascript/komado.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <title>小窓</title>
+</head>
+<body>
+  <script type="text/javascript">
+    function openWin() {
+        window.open(
+            "https://programmercollege.jp/",
+            "_blank",
+            "menubar=0,width=300,height=200,top=100,left=100"
+        );
+    }
+    </script>
+    <div style="background-color :#CCC;">
+        <input type="button" value="小窓を開く" onClick="openWin()">　　
+    </div>
+</body>
+</html>
